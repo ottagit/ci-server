f8eb32b77572a333b121ae1c979060afd44f89be
ottagit <ottaosawa@gmail.com>
2023-12-13 11:45:41 +0300

    deploy EC2 instance for CI/CD server installation

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Tell Terraform which service to use for resource setup
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+      Owner     = "Chris Otta"
+    }
+  }
+}
+
+module "jenkins_server" {
+  source = "github.com/ottagit/modules//ci-cd/jenkins-server?ref=v0.0.3"
+
+  ami_name          = "Jenkins instance"
+  ami_id            = "ami-0230bd60aa48260c6"
+  ami_key_pair_name = "testenv"
+}
+
+terraform {
+  backend "s3" {
+    key = "stage/ci-servers/jenkins/terraform.tfstate"
+
+    bucket = "batoto-bitange"
+    region = "us-east-1"
+
+    dynamodb_table = "terraone-locks"
+    encrypt        = true
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "elastic_ip" {
+  value       = module.jenkins_server.public_ip
+  description = "The elastic IP of the EC2 Jenkins server"
+}
