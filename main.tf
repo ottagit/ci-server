06d13514627a08bfb2a907a19a578f86c6b08ad8
ottagit <ottaosawa@gmail.com>
2023-12-30 22:38:06 +0300

    remove role name parameter from required list

--- main.tf
+++ main.tf
@@ -11,12 +11,11 @@ provider "aws" {
 }
 
 module "jenkins_server" {
-  source = "github.com/ottagit/modules//ci-cd/jenkins-server?ref=v0.0.6"
+  source = "github.com/ottagit/modules//ci-cd/jenkins-server?ref=v0.0.7"
 
   ami_name          = "Jenkins Instance"
   ami_id            = "ami-0230bd60aa48260c6"
   ami_key_pair_name = "testenv"
-  name              = "jenkins-instance"
 }
 
 terraform {
